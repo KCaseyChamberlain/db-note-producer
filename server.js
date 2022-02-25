8767d8b681c19501c83c985614c7c9342e567df5
KCaseyChamberlain <caseygchamberlain@gmail.com>
2022-02-25 16:17:22 -0700

    notes.html route issue

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const path = require("path");
 const { notes } = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/api/notes', (req, res) => {
     res.json(notes);
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/assets/notes.html"));
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, './public/assets/index.html'));
 });
 
 
