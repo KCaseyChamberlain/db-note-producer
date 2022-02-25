6a204c1985452cceb73d015a8c288b7118c49be5
KCaseyChamberlain <caseygchamberlain@gmail.com>
2022-02-25 16:29:30 -0700

    html files are visible through server

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/assets/notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, './public/assets/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 
