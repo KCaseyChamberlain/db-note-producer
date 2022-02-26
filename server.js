f6a82ed7d03a4fefa0f77fc8b0ddf109fcd614de
KCaseyChamberlain <caseygchamberlain@gmail.com>
2022-02-26 01:32:43 -0700

    app.port is functional

--- server.js
+++ server.js
@@ -6,10 +6,18 @@ const { notes } = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
+});
+
+app.post("/api/notes", (req, res) => {
+    // req.body is where our incoming content will be
+    console.log(req.body);
+    res.json(req.body);
 });
 
 app.get("/notes", (req, res) => {
@@ -19,26 +27,6 @@ app.get("/notes", (req, res) => {
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
-
-
-
-app.post('/notes', (req, res) => {
-    // req.body is where our incoming content will be
-    console.log(req.body);
-    res.json(req.body);
-  });
-
-//   app.post("/notes", (req, res) => {
-//     req.body.id = notes.length.toString();
-
-//     // if (!validateNotes(req.body)) {
-//     //     res.status(400).send("The notes is not properly formatted.");
-//     // } else {
-//     //     const notes = createNewNotes(req.body, notes);
-//     //     res.json(notes);
-//     // }
-// });
-
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
