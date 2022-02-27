023b502c98707793a4d0a91b7092a41c58259301
KCaseyChamberlain <caseygchamberlain@gmail.com>
2022-02-27 16:49:40 -0700

    delete route functional, final draft

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## DB Note Producer
 
 ### Description
-This application dynamically creates notes using API routes, hosted on a server provided by Heroku.
+This application dynamically creates AND deletes notes using API routes. Hosted on a server provided by Heroku.
 
 ### License
 ![Github licence](https://img.shields.io/badge/license-MIT-blue.svg)
@@ -24,6 +24,6 @@ No tests at this time.
 ### Questions
 GitHub Username: (kcaseychamberlain) 
     
-View the project in Heroku at: https://db-note-producer.herokuapp.com/
+View project hosted by Heroku at: https://db-note-producer.herokuapp.com/
     
 If you have any questions, contact Casey at: caseygchamberlain@gmail.com
--- server.js
+++ server.js
@@ -7,12 +7,13 @@ var dataBase = path.join(__dirname, "./db/db.json");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.json());
 
-// HTML ROUTES
-app.get("/", (req, res) => {
+// HTML routes
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
@@ -20,17 +21,18 @@ app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
+// API route
 app.get('/api/notes', (req, res) => {
     fs.readFile(dataBase, "utf-8", function (err, noteValues) {
         if (err) throw err
         else {
-            // console.log("FETCHING NOTES!")
             noteValues = JSON.parse(noteValues)
             return res.json(noteValues)
         }
     })
 });
 
+// ID route
 app.get('/api/notes/:id', (req, res) => {
     var selectedNote = req.params.id
     for (var i = 0; i < notes.length; i++) {
@@ -40,40 +42,40 @@ app.get('/api/notes/:id', (req, res) => {
     }
 });
 
+// delete note route
 app.delete('/api/notes/:id', (req, res) => {
-    fs.readFile(dataBase, "utf-8", function (err, noteValues) {
+    fs.readFile(dataBase, "utf-8", function (err) {
         if (err) throw err
         else {
-            console.log("READING NOTES FOR DELETION!")
-            noteValues = JSON.parse(noteValues)
-            return res.json(noteValues)
+            console.log("Reading notes for deletion")
         }
     })
     var selectedNote = req.params.id
     for (var i = 0; i < notes.length; i++) {
         if(selectedNote == notes[i].id){
-            delete notes[selectedNote]
-            console.log(selectedNote)
-            console.log(notes)
+            for (var i = notes.length - 1; i >= 0; --i) {
+                if (notes[i].id == selectedNote) {
+                    notes.splice(i,1);
+                    console.log(notes)
+                }
+            }
         }
-        // if(notes = null){
-        //     delete null
-        // }
     }
-    fs.writeFile(dataBase, JSON.stringify(notes), function (err) {
+    fs.writeFile(dataBase, JSON.stringify(notes), function (err, noteValues) {
         if (err) throw err
         else {
-            console.log("note has been deleted!")
+            return res.json(noteValues)
         }
     })
 });
 
+// post note route
 app.post("/api/notes", (req, res) => {
     var note = req.body;
     fs.readFile(dataBase, "utf-8", function (err, res) {
         if (err) throw err
         else {
-            console.log("POSTING NOTES!")
+            console.log("Posting note!")
             notes = JSON.parse(res)
             notes.push(note)
             for (var i = 0; i < notes.length; i++) {
@@ -82,7 +84,7 @@ app.post("/api/notes", (req, res) => {
             fs.writeFile(dataBase, JSON.stringify(notes), function (err) {
                 if (err) throw err
                 else {
-                    console.log("note has been added bossman!")
+                    console.log("Note has been added bossman!")
                 }
             })
         }
